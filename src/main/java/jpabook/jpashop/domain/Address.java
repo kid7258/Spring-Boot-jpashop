3ca90fe2d1d61cc75a4e75a40b229bdf4c1d7c8f
kid7258 <kid7258@naver.com>
2022-05-19 21:23:21 +0900

    주문 도메인 개발

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,9 +1,13 @@
 package jpabook.jpashop.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 @Getter
 public class Address {
